e44542f86ebe27d62db76e71f16aa59687800997
mrpopos <1213167844@qq.com>
2024-05-18 16:53:44 +0800

    antd-mobile组件库主题样式定制

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import '@/index.css';
 import router from '@/router'
 import { RouterProvider } from 'react-router-dom';
 
+// 导入定制主题
+import './theme.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RouterProvider router={router} />
--- src/page/Layout/index.js
+++ src/page/Layout/index.js
@@ -1,10 +1,15 @@
+import { Button } from "antd-mobile"
 import { Outlet } from "react-router-dom"
 
 const Layout = () => {
   return (
     <div>
-      <h1>layout 组件</h1>
       <Outlet />
+      <Button color="primary">测试全局</Button>
+      <div className="puple">
+        <Button color="primary">测试局部</Button>
+      </div>
+      <h1>layout 组件</h1>
     </div>
   )
 }
