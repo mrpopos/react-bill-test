f146782fb771b8e3183694dfa43933c782b7f221
DESKTOP-HMMAGF9\tusl <mrpopos.wolton@gmail.com>
2024-05-22 00:35:44 +0800

    样式初始化，月度支出菜单编写

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'reset-css'
 import '@/index.css';
+// 导入定制主题
+import './theme.css'
 import router from '@/router'
 import { RouterProvider } from 'react-router-dom';
 import store from '@/store'
 import { Provider } from 'react-redux';
-
-// 导入定制主题
-import './theme.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/page/Month/index.js
+++ src/page/Month/index.js
@@ -1,7 +1,54 @@
+import { useState } from 'react';
+import { NavBar,DatePicker, Toast } from 'antd-mobile'
+
+import './index.scss'
+
 const Month = () => {
+  const [visible, setVisible] = useState(false)
+
   return (
-    <div>
-      <h1>Month 组件</h1>
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>月度支出</NavBar>
+      <div className="content">
+        <div className='header'>
+          {/* 时间切换区域 */}
+          <div className='date'>
+            <span className='text'>
+              2023 | 3月份
+            </span>
+            <span className='arrow expand'></span>
+          </div>
+          {/* 统计区域 */}
+          <div className='twoLineOverview'>
+            <div className='item'>
+              <span className='money'>{100}</span>
+              <span className='type'>支出</span>
+            </div>
+            <div className='item'>
+              <span className='money'>{100}</span>
+              <span className='type'>收入</span>
+            </div>
+            <div className='item'>
+              <span className='money'>{100}</span>
+              <span className='type'>结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className='kaDate'
+            title='记账日期'
+            visible={visible}
+            max={new Date()}
+            precision="month"
+            onClose={() => {
+              setVisible(false)
+            }}
+            onConfirm={val => {
+              Toast.show(val.toDateString())
+            }}
+          />
+        </div>
+      </div>
     </div>
   )
 }
